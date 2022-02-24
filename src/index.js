52a37c9113f2c2ed82e7fa13cc950a78eb86f7c5
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-02-24 15:15:50 -0300

    feat: create checksTodoExists middleware

--- src/index.js
+++ src/index.js
@@ -31,7 +31,27 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({error: "Invalid UUID!"});
+  }
+
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({error: "User not found!"});
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({error: "ToDo not found!"});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
